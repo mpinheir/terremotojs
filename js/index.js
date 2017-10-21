63dd048e4f4ca22979d9a08950da1b1d0ebece33
Victor Chastinet <Victorchastinet@live.com>
2017-10-21 01:13:40 -0200

    Basic changes (#1)
    * created separated files for the JS and CSS and organized the HTML to follow good practice
    
    * added some bootstrap styles to centralize the table and highlight the title
    
    * moved the 'styles' to a css file
    
    * minor syntax correction
    
    * added some styles and changed the units to 'rem' (it's more responsive)
    
    * made the html more semantic and a minor change to the JS for better styling control
    
    * removed an extra js file (not used, for now)

---
+++ js/index.js
@@ -0,0 +1,40 @@
+var xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        
+        var totalEarthquakes = parseInt(myObj.metadata.count);
+        //Prints total number of Earthquakes read from USGS.gov site.
+        document.querySelector(".summary span").innerHTML = myObj.metadata.count;
+        
+        //assembles table containing data pertaining to each earhquake
+        //assembles table heading
+        var myTable="<table class='table table-striped'>";
+        
+        myTable = myTable + "<thead><tr><th>Location</th><th>Magnitude</th><th>Longitude</th><th>Latitude</th><th>Depth</th></tr></thead>";
+
+
+        //assembles table rows
+        myTable = myTable + "<tbody>";
+        for( var i = 0; i < totalEarthquakes; i++ ){
+            //adds place and magnitude to table row
+            myTable = myTable + "<tr><td>" + myObj.features[i].properties.place + "</td><td>" + myObj.features[i].properties.mag + "</td>";
+
+            //adds latitude and madnitude to table row
+            myTable = myTable + "<td>" + myObj.features[i].geometry.coordinates[0] + "</td><td>" + myObj.features[i].geometry.coordinates[1] + "</td>";
+
+            //adds latitude and madnitude to table row
+            myTable = myTable + "<td>" + myObj.features[i].geometry.coordinates[2] + " Km " + "</td></tr>";
+
+        }
+        
+        myTable = myTable + "</tbody></table>";
+
+        //displays the assembled table
+        document.getElementById('tablePrint').innerHTML = myTable;
+    }
+};
+
+xmlhttp.open("GET", "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson", true);
+xmlhttp.send();
