b30f3cdb96b93f704ed8024f77d907315843f4d5
Vchastinet <victorchastinet@live.com>
2017-11-08 02:08:31 -0300

    the magnitude is now colored to indicate de alert level

--- js/tools.js
+++ js/tools.js
@@ -23,30 +23,29 @@ function sortBy(criteria, data, order){
 function assembleTable(data){
   
   //assembles table rows
-  let table = "<tr>";
+  let table = '<tr>';
   for( var i = 0; i < data.length; i++ ){
-    
       //adds place  to table row
-      table += "<td>" + data[i].properties.place + "</td>";
+      table += '<td>' + data[i].properties.place + '</td>';
       //adds date to table row
       let date = new Date(data[i].properties.time);
-      table += "<td>" + date.toDateString() + "</td>";
+      table += '<td>' + date.toDateString() + '</td>';
 
       //adds magnitude table row
-      table += "<td>" + data[i].properties.mag + "</td>";
+      table += '<td style="color:' + data[i].properties.alert + '; font-weight: bold">' + data[i].properties.mag + '</td>';
 
       //adds longitude and latitude to table row
-      table += "<td>" + data[i].geometry.coordinates[0] + "</td><td>" + data[i].geometry.coordinates[1] + "</td>";
+      table += '<td>' + data[i].geometry.coordinates[0] + '</td><td>' + data[i].geometry.coordinates[1] + '</td>';
 
       //adds depth to table row
-      table += "<td>" + data[i].geometry.coordinates[2] + " Km " + "</td></tr>";
+      table += '<td>' + data[i].geometry.coordinates[2] + ' Km' + '</td></tr>';
 
   }
   return table;
 }
 
 function updateTable(criteria, data, order){
-  data = sortBy(criteria.getAttribute("value"), data, order);
-  document.querySelector('.table tbody').innerHTML = "";
+  data = sortBy(criteria.getAttribute('value'), data, order);
+  document.querySelector('.table tbody').innerHTML = '';
   document.querySelector('.table tbody').innerHTML = assembleTable(data);
 }
