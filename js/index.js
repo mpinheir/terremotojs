8a2afbcbe3ea433f84d40da1fa832f9bf5ba57e9
Victor Chastinet <Victorchastinet@live.com>
2017-11-11 22:40:28 -0200

    Sorting feature (#3)
    * created separated files for the JS and CSS and organized the HTML to follow good practice
    
    * added some bootstrap styles to centralize the table and highlight the title
    
    * moved the 'styles' to a css file
    
    * minor syntax correction
    
    * added some styles and changed the units to 'rem' (it's more responsive)
    
    * made the html more semantic and a minor change to the JS for better styling control
    
    * removed an extra js file (not used, for now)
    
    * added a "date" collumn to the table
    
    * minor syntax fix
    
    * implemented the "sort" feature, created a new JS file with functions to implement the feature
    
    * updating file
    
    * the magnitude is now colored to indicate de alert level
    
    * changed the alert indicator
    
    * centralized content of the magnitude column
    
    * enhanced the accuracy when sorting by location,
    
    * minor syntax cleaning
    
    * added some coments

--- js/index.js
+++ js/index.js
@@ -3,45 +3,37 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
+
+        var earthquakesInfo = myObj.features;
         
         var totalEarthquakes = parseInt(myObj.metadata.count);
         //Prints total number of Earthquakes read from USGS.gov site.
-        document.querySelector(".summary span").innerHTML = myObj.metadata.count;
-        
+        document.querySelector('.summary span').innerHTML = myObj.metadata.count;
         //assembles table containing data pertaining to each earhquake
         //assembles table heading
-        var myTable="<table class='table table-striped'>";
-        
-        myTable = myTable + "<thead><tr><th>Location</th><th>Date</th><th>Magnitude</th><th>Longitude</th><th>Latitude</th><th>Depth</th></tr></thead>";
+        var myTable = '<table class="table table-striped">';
 
-        //assembles table rows
-        myTable = myTable + "<tbody>";
-        for( var i = 0; i < totalEarthquakes; i++ ){
-          
-            //adds place  to table row
-            myTable = myTable + "<tr><td>" + myObj.features[i].properties.place + "</td>";
-
-            //adds date to table row
-            let date = new Date(myObj.features[i].properties.time);
-            myTable = myTable + "<td>" + date.toDateString() + "</td>";
-
-            //adds magnitude table row
-            myTable = myTable + "</td><td>" + myObj.features[i].properties.mag + "</td>";
-
-            //adds longitude and latitude to table row
-            myTable = myTable + "<td>" + myObj.features[i].geometry.coordinates[0] + "</td><td>" + myObj.features[i].geometry.coordinates[1] + "</td>";
-
-            //adds depth to table row
-            myTable = myTable + "<td>" + myObj.features[i].geometry.coordinates[2] + " Km " + "</td></tr>";
-
-        }
-        
-        myTable = myTable + "</tbody></table>";
-
+        //assembles table containing data pertaining to each earhquake
+        //assembles table heading
+        myTable += '<thead><th value="place">Location</th><th value="time">Date</th><th value="mag">Magnitude</th><th value="0">Longitude</th><th value="1">Latitude</th><th value="2">Depth</th></thead><tbody>';
+     
+        myTable += assembleTable(earthquakesInfo) + '</tbody></table>';
         //displays the assembled table
         document.getElementById('tablePrint').innerHTML = myTable;
+
+        //select the criteria for sorting
+        var tableHeads = document.querySelectorAll('.table th');
+
+        var order = "time";
+        //add the click event to the table heads
+        tableHeads.forEach(function(head){
+            head.onclick = function(){
+                updateTable(head, earthquakesInfo, order);
+                order = head.getAttribute('value');
+            }
+        });
     }
 };
 
-xmlhttp.open("GET", "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson", true);
+xmlhttp.open('GET', 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson', true);
 xmlhttp.send();
